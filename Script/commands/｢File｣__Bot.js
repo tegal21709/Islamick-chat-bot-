8693109a6ecff8d30f4673cb51971d7ce2db24a6
tegal21709 <didar8didar123@gmail.com>
2025-04-16 14:38:54 +0600

    Update ｢File｣__Bot.js

--- Script/commands/｢File｣__Bot.js
+++ Script/commands/｢File｣__Bot.js
@@ -195,7 +195,7 @@ module.exports.handleEvent = async function({ api, event, args, Threads, Users }
      return api.sendMessage("হুম ঝাং আমি তোমাকে রাইতে ভলোপাসি <🥵", threadID);
    };
 
-   if ((event.body.toLowerCase() == "ami ullash") || (event.body.toLowerCase() == "kire")) {
+   if ((event.body.toLowerCase() == "ami Didar") || (event.body.toLowerCase() == "kire")) {
      return api.sendMessage("হ্যা বস কেমন আছেন..?☺️", threadID);
    };
   mess = "{name}"
